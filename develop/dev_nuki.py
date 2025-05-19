5f2cf0bd44d69d713cf79daa87aefc071941c915
Kolja Wagner <k.wagner@stud.uni-hannover.de>
2025-05-19 17:13:02 +0200

    clean up development scripts

---
+++ develop/dev_nuki.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  6 22:12:08 2025
+
+@author: kolja
+"""
+import logging
+from pprint import pprint
+from typing import Optional, Union, List
+
+from lh_core import config
+from lh_core import Nuki
+
+def test():
+    logging.info("syncronous")    
+    config.load_config("config_dev.cfg")
+    nuki = Nuki()
+    nuki.retrieve_smartlock_ids()
+    nuki.set_default_lock()
+    
+    res = nuki.get_logs(lock_id=None, raw=False, limit=2)
+    # res = nuki.get_smartlock(raw=True)
+    pprint(res)
+    
+
+    
+
+if __name__ == "__main__":
+    test()
+    
